d7e55dd093a0b69903abdd922f3c99f5fc992fde
seadaemon <hidden@mailfence.com>
2024-11-01 15:57:03 -0400

    Update vk_engine.h

--- gpbr/include/gpbr/Graphics/Vulkan/vk_engine.h
+++ gpbr/include/gpbr/Graphics/Vulkan/vk_engine.h
@@ -3,17 +3,50 @@
 #include "vk_types.h"
 #include "VkBootstrap.h"
 
+struct FrameData
+{
+    VkSemaphore _swapchainSemaphore, _renderSemaphore;
+    VkFence _renderFence;
+
+    VkCommandPool _commandPool;
+    VkCommandBuffer _mainCommandBuffer;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
 class VulkanEngine
 {
   public:
     bool _is_initialized{false};
     int _frame_number{0};
-    bool _stop_rendering{false};
     VkExtent2D _window_extent{1700, 900};
 
     struct SDL_Window* _window{nullptr};
 
     static VulkanEngine& get();
+
+    vkb::InstanceDispatchTable _instance_dispatch_table;
+    vkb::DispatchTable _dispatch_table;
+
+    VkInstance _instance;                      // Vulkan library handle
+    VkDebugUtilsMessengerEXT _debug_messenger; // debug output handle
+    VkPhysicalDevice _chosen_GPU;              // selected GPU
+    VkDevice _device;                          // device for commands
+    VkSurfaceKHR _surface;                     // window surface
+
+    FrameData _frames[FRAME_OVERLAP];
+
+    FrameData& get_current_frame() { return _frames[_frame_number % FRAME_OVERLAP]; };
+
+    VkQueue _graphics_queue;
+    uint32_t _graphics_queue_family;
+
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapchain_image_format;
+
+    std::vector<VkImage> _swapchain_images;
+    std::vector<VkImageView> _swapchain_image_views;
+    VkExtent2D _swapchain_extent;
 
     // initializes everything in the engine
     void init();
@@ -27,28 +60,16 @@ class VulkanEngine
     // run main loop
     void run();
 
-    vkb::InstanceDispatchTable _instance_dispatch_table;
-    vkb::DispatchTable _dispatch_table;
-
-    VkInstance _instance;                      // Vulkan library handle
-    VkDebugUtilsMessengerEXT _debug_messenger; // debug output handle
-    VkPhysicalDevice _chosen_GPU;              // selected GPU
-    VkDevice _device;                          // device for commands
-    VkSurfaceKHR _surface;                     // window surface
-
-    VkSwapchainKHR _swapchain;
-    VkFormat _swapchain_image_format;
-
-    std::vector<VkImage> _swapchain_images;
-    std::vector<VkImageView> _swapchain_image_views;
-    VkExtent2D _swapchain_extent;
+    bool _stop_rendering{false};
 
   private:
     void init_vulkan();
     void init_swapchain();
-    void init_commands();
-    void init_sync_structures();
 
     void create_swapchain(uint32_t width, uint32_t height);
     void destroy_swapchain();
+
+    void init_commands();
+
+    void init_sync_structures();
 };
